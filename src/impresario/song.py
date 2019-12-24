cff9b55395c8245661688c03e086a2b0d738b655
Kip Yin <28321392+kipyin@users.noreply.github.com>
2019-12-24 23:50:59 +0800

    Clean up the code

--- src/impresario/song.py
+++ src/impresario/song.py
@@ -1,15 +1,14 @@
+from copy import deepcopy
 from pathlib import Path
-from typing import List
+from typing import ClassVar, List, Sequence, Tuple
 
 import attr
 import tablib
 from pypinyin import lazy_pinyin
 
-from . import theory
+from impresario import theory
 
 PINYIN_ADJUSTMENTS = {ord("祢"): "nǐ,mí"}
-
-SITE_DIR = "/Users/kip/projects/lego-songbook"
 
 
 @attr.s(auto_attribs=True, slots=True)
@@ -45,21 +44,16 @@ class Song:
 
 @attr.s(auto_attribs=True, slots=True)
 class Songbook:
+    """A collection of songs."""
 
-    _data: tablib.Dataset
-    _sort_by: str = "name"
+    HEADERS: ClassVar[List[str]] = ["name", "key", "hymn_ref", "sheet_type"]
+    _data: Sequence[Tuple[str, str, int, str]]
+    _sort_by: str = None
+    _dataset: tablib.Dataset = attr.ib(init=False)
 
     def __attrs_post_init__(self):
+        self._dataset = tablib.Dataset(*self._data, headers=self.HEADERS)
         self.sort(by=self._sort_by)
-        # if self._sort_by == "name":
-        #     key = self._sort_by_name
-        # elif self._sort_by == "key":
-        #     key = self._sort_by_key
-        # else:
-        #     raise ValueError("Can only sort by names or keys.")
-        # self._data.append_col(key, "sort_col")
-        # self._data = self._data.sort("sort_col")
-        # del self._data["sort_col"]
         return True
 
     @staticmethod
@@ -70,22 +64,30 @@ class Songbook:
 
     @staticmethod
     def _sort_by_key(row):
-        return [lazy_pinyin(row[0].split(" ")), theory.key_order(row[1])][::-1]
+        name_pinyin = lazy_pinyin(row[0].split(" "))
+        title = [name.title() for name in name_pinyin]
+        return [lazy_pinyin(title), theory.key_order(row[1])][::-1]
 
     @property
     def songs(self) -> List[Song]:
         """Get the songs in the songbook."""
         songs_list = []
-        for song in self._data.dict:
+        for song in self._dataset.dict:
             songs_list.append(Song.from_dict(**song))
         return songs_list
+
+    @property
+    def csv(self) -> str:
+        return self._dataset.csv
 
     @classmethod
     def from_file(cls, data_source) -> "Songbook":
         """Create a ``Songbook`` instance from a csv file."""
-        return cls(
-            tablib.Dataset().load(open(data_source, "r", encoding="utf-8").read())
-        )
+        dataset = tablib.Dataset().load(open(data_source, "r", encoding="utf-8").read())
+        songs = []
+        for row in dataset:
+            songs.append(row)
+        return cls(songs)
 
     @classmethod
     def from_sheets(cls, sheets_folder: str) -> "Songbook":
@@ -94,37 +96,39 @@ class Songbook:
         if not sheets_folder.exists():
             raise ValueError(f"{sheets_folder} is invalid.")
 
-        headers = ["name", "sheet_type"]
-        data = tablib.Dataset(headers=headers)
+        data = []
 
         for file in sheets_folder.iterdir():
             name, ext = file.name.split(".")
             if name == "" or ext.lower() not in ["pdf", "jpg", "png", "gif"]:
                 continue
-            data.append((name, ext))
+            data.append((name, "", "", ext))
 
         return cls(data)
 
     def sort(self, by: str) -> bool:
         """Add a dynamic column to the data, and sort the column by ``key`` function."""
-        if by == "name":
+        if by is None:
+            return False
+        elif by == "name":
             key_func = self._sort_by_name
         elif by == "key":
             key_func = self._sort_by_key
         else:
             raise ValueError("Can only sort by names or keys.")
-        self._data.append_col(key_func, "sort_col")
-        self._data = self._data.sort("sort_col")
-        del self._data["sort_col"]
+        self._dataset.append_col(key_func, "sort_col")
+        self._dataset = self._dataset.sort("sort_col")
+        del self._dataset["sort_col"]
         return True
 
-    def dump_songs(self, target_file: str) -> bool:
+    def export(self, target_file: str, sort_by: str) -> bool:
         target_file = Path(target_file)
         target_file.touch(exist_ok=True)
-        target_file.write_text(self._data.csv, "utf-8")
+        self.sort(sort_by)
+        target_file.write_text(self._dataset.csv, "utf-8")
         return True
 
-    def merge(self, other: "Songbook", update=True, ignore_empty=True) -> bool:
+    def merge(self, other: "Songbook", update=True, ignore_empty=True) -> "Songbook":
         """Synchronize this songbook with another songbook.
 
         If ``update`` is ``True``, then keep the name form ``self`` and
@@ -132,54 +136,48 @@ class Songbook:
         """
         if not isinstance(other, Songbook):
             raise TypeError(f"Cannot merge a `Songbook` with a(n) {type(other)}.")
-        if self._data.headers != self._data.headers:
+        if self._dataset.headers != self._dataset.headers:
             raise ValueError("Two songbooks' headers must match in order to merge.")
 
-        for row in other._data.dict:
-            if row["name"] in self._data["name"] and update:
-                ...
-            elif row["name"] in self._data["name"] and not update:
-                ...
+        new_book = deepcopy(self)  # Hack.
+
+        # Do a line by line analysis.
+        # If `update` is True, then go ahead and update the data from
+        # the other Songbook. If `update` is False, continue.
+
+        for i, other_row in enumerate(other._dataset.dict):
+            if other_row["name"] in self._dataset["name"] and update:
+                new_row_index = new_book._dataset["name"].index(other_row["name"])
+                new_row: List[str, str, int, str] = list(
+                    new_book._dataset[new_row_index]
+                )
+                if ignore_empty:
+                    for j, (header, value) in enumerate(other_row.items()):
+                        if value:
+                            new_row[j] = value
+                        else:
+                            pass
+                else:
+                    new_row = other_row.values()
+                new_book._dataset.insert(index=new_row_index, row=new_row)
+                del new_book._dataset[new_row_index + 1]
             else:
-                ...
-        self._data = self._data.stack(other._data)  # Hacky. Dataset().dict returns a
-        # list
-        # of ordered dicts, which does not quite work in this case.
-        self._data.remove_duplicates()
-        self.sort(by=self._sort_by)
-        return True
+                new_book._dataset.append(other._dataset[i])
+
+        new_book._dataset.remove_duplicates()
+        new_book.sort(by=new_book._sort_by)
+        return new_book
 
 
-# def add_sheet_ext(row):
-#     local_songs = get_local_songs("/Users/kip/Documents/LEGO/Lego Songbook")
-#     return local_songs.get(row[0], "")
-#
-#
-# def change_sheet_to_ext(songs: tablib.Dataset, path: str) -> tablib.Dataset:
-#     local_songs = get_local_songs(path)
-#     songs.append_col(add_sheet_ext, "sheet_type")
-#     del songs["sheet_type"]
-#     return songs
+if __name__ == "__main__":
+    SITE_DIR = "/Users/kip/projects/lego-songbook"
+    SONGS_CSV = Path(SITE_DIR) / "_data/songs.csv"
+    SONGS_BY_KEY_CSV = Path(SITE_DIR) / "_data/songs_by_key.csv"
+    SONGBOOK_DIR = "/Users/kip/Documents/LEGO/Lego Songbook"
 
-
-# def sync_local_songs(songs: tablib.Dataset, path: str) -> tablib.Dataset:
-#     local_songs = get_local_songs(path)
-#     for local_song, ext in local_songs.items():
-#         if local_song not in songs["name"]:
-#             songs.append([local_song, "", "--", ext])
-#     return songs
-
-
-# if __name__ == "__main__":
-#     SONGS_CSV = Path(SITE_DIR) / "_data/songs.csv"
-#     SONGS_BY_KEY_CSV = Path(SITE_DIR) / "_data/songs_by_key.csv"
-#     SONGBOOK_DIR = "/Users/kip/Documents/LEGO/Lego Songbook"
-#     # dump_songs(sort_songs_by_pinyin(load_songs(SONGS_CSV)), SONGS_BY_KEY_CSV)
-#     # print(key_order(key_conversion("G#m")))
-#     # print(get_local_songs("/Users/kip/Documents/LEGO/Lego Songbook"))
-#     # print(change_sheet_to_ext(load_songs(SONGS_CSV), SONGBOOK_DIR))
-#     # dump_songs(sort_songs(sync_local_songs(load_songs(SONGS_CSV), SONGBOOK_DIR),
-#     #                       sort_by_name),
-#     #            SONGS_CSV)
-#     # dump_songs(sort_songs(load_songs(SONGS_CSV), sort_by_key), SONGS_BY_KEY_CSV)
-#     pass
+    songbook = Songbook.from_file(SONGS_CSV)
+    songbook_sheets = Songbook.from_sheets(SONGBOOK_DIR)
+    merged_songbook = songbook.merge(songbook_sheets)
+    merged_songbook.sort("name")
+    merged_songbook.export(SONGS_CSV, sort_by="name")
+    merged_songbook.export(SONGS_BY_KEY_CSV, sort_by="key")
--- tests/test_song.py
+++ tests/test_song.py
@@ -6,22 +6,32 @@ from impresario.song import Song, Songbook
 
 @pytest.fixture(scope="function")
 def songbook():
-    data = tablib.Dataset(headers=["name", "key", "hymn_ref", "sheet_type"])
+    # data = tablib.Dataset(headers=["name", "key", "hymn_ref", "sheet_type"])
     songs = [("歌曲 A", "Bb", 3, "PDF"), ("歌曲 B", "C", 10, "PNG")]
-    for song in songs:
-        data.append(song)
+    # for song in songs:
+    #     data.append(song)
 
-    yield Songbook(data, sort_by="name")
+    yield Songbook(songs, sort_by="name")
 
 
 @pytest.fixture(scope="function")
 def another_songbook():
-    data = tablib.Dataset(headers=["name", "key", "hymn_ref", "sheet_type"])
+    # data = tablib.Dataset(headers=["name", "key", "hymn_ref", "sheet_type"])
     songs = [("歌曲 C", "A", "--", "GIF"), ("歌曲 D", "G", 400, "JPG")]
-    for song in songs:
-        data.append(song)
+    # for song in songs:
+    #     data.append(song)
 
-    yield Songbook(data, sort_by="key")
+    yield Songbook(songs, sort_by="key")
+
+
+@pytest.fixture(scope="function")
+def songbook_with_empty_fields():
+    # data = tablib.Dataset(headers=["name", "key", "hymn_ref", "sheet_type"])
+    songs = [("歌曲 A", "", 0, "GIF")]
+    # for song in songs:
+    #     data.append(song)
+
+    yield Songbook(songs)
 
 
 def test_song_pinyin():
@@ -42,7 +52,7 @@ def test_song_normalized_key():
 def test_songbook_sanity():
     data = tablib.Dataset(headers=["name", "key", "sheet_type"])
     data.append(("歌曲", "C", "PDF"))
-    Songbook(data)
+    Songbook([("歌曲", "C", 0, "PDF")])
 
 
 def test_songbook_get_songs(songbook):
@@ -68,12 +78,23 @@ def test_songbook_sort_by_key(songbook):
     ]
 
 
-@pytest.mark.xfail
 def test_songbook_merge(songbook, another_songbook):
-    songbook.merge(another_songbook)
-    assert songbook.songs == [
+    new_songbook = songbook.merge(another_songbook)
+    assert new_songbook.songs == [
         Song("歌曲 A", "Bb", 3, "PDF"),
         Song("歌曲 B", "C", 10, "PNG"),
         Song("歌曲 C", "A", "--", "GIF"),
         Song("歌曲 D", "G", 400, "JPG"),
+    ]
+    assert songbook.songs == [
+        Song("歌曲 A", "Bb", 3, "PDF"),
+        Song("歌曲 B", "C", 10, "PNG"),
+    ]  # the original copy stays the same
+
+
+def test_songbook_merge_with_empty_fields(songbook, songbook_with_empty_fields):
+    new_songbook = songbook.merge(songbook_with_empty_fields, ignore_empty=False)
+    assert new_songbook.songs == [
+        Song("歌曲 A", "", 0, "GIF"),
+        Song("歌曲 B", "C", 10, "PNG"),
     ]
