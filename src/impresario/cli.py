3ebaae8894f226aa9b25d6797d18433f765463c3
Kip Yin <28321392+kipyin@users.noreply.github.com>
2019-12-26 00:20:12 +0800

    Add CLI

--- src/impresario/cli.py
+++ src/impresario/cli.py
@@ -1,2 +1,42 @@
 import click
 
+from impresario import song
+
+
+@click.group()
+def main():
+    pass
+
+
+@click.group()
+def songbook():
+    pass
+
+
+@click.group()
+def setlist():
+    pass
+
+
+@songbook.command("sync")
+@click.option("--site", required=True)
+@click.option("--sheet", required=True)
+def _songbook_sync(site, sheet):
+    click.echo(f"Site directory: {site}")
+    click.echo(f"Sheet directory: {sheet}")
+    if song.sync_songbook(site, sheet):
+        click.echo("Successfully synced the songbooks!")
+        return
+    else:
+        click.echo("Fail to sync the songbooks.")
+
+
+@songbook.command("missing")
+@click.option("--site", required=True)
+@click.option("-c", "--column", required=True)
+def _songbook_show_missing(site, column):
+    click.echo(song.show_missing_songs(site, column))
+
+
+main.add_command(songbook)
+main.add_command(setlist)
